87809e0241a26358b3c7444a4577c2427381ff4d
Luke Manyamazi <isirluke@gmail.com>
2025-02-20 22:44:24 +0200

    added bg and aligned icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/kevinRushLogo.png";
+import logo from "../assets/lukeManyamaziLogo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
@@ -6,15 +6,15 @@ import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+    <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="kr letters in white color" className="mx-2 w-14" />
       </div>
-      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl text-neutral-300">
         <FaLinkedin />
         <FaGithub />
-        <FaSquareXTwitter />
         <FaInstagram />
+        <FaSquareXTwitter />
       </div>
     </nav>
   );
