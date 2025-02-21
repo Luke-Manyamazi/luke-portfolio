79f89aeb26d64147b5803203a57122b314320d07
Luke Manyamazi <isirluke@gmail.com>
2025-02-22 01:38:12 +0200

    added motion to h1 and fixed the center alignment

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,21 @@ import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
-      <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2">
-          <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl text-neutral-300">
+      <div className="flex items-center justify-center ">
+        {" "}
+        {/* Ensures the content takes up full height of the screen */}
+        <div className="flex flex-wrap w-full max-w-7xl px-4">
+          {" "}
+          {/* Added padding to avoid edge touch */}
+          <div className="w-full lg:w-1/2 flex flex-col items-center lg:items-start">
+            <motion.h1
+              initial={{ x: -100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 1 }}
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl text-neutral-300"
+            >
               Luke Manyamazi
-            </h1>
+            </motion.h1>
             <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
               Software Engineer & FSD - Trainee
             </span>
@@ -18,15 +27,13 @@ const Hero = () => {
               {HERO_CONTENT}
             </p>
           </div>
-        </div>
-        <div className="w-full lg:w-1/2 lg:p-8">
-          <div className="flex justify-center py-10">
+          <div className="w-full lg:w-1/2 lg:p-8 flex justify-center py-10">
             <img
               src={profilePic}
               width={400}
               height={400}
               alt="Luke Manyamazi on a red background"
-              className="rounded-2xl brightness-90 bg-gradient-to-tl from-black via-transparent to-black opacity-50"
+              className="rounded-2xl brightness-90 bg-gradient-to-tl from-black via-transparent to-black opacity-50 object-cover"
             />
           </div>
         </div>
