db7ea67630ffbcb2fef04087af40b6c9627e5595
Luke Manyamazi <isirluke@gmail.com>
2025-02-22 10:34:15 +0200

    added motion to the technologies

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,15 @@ import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/lukeManyamaziProfile.jpg";
 import { motion } from "framer-motion";
 
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
+
 const Hero = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
@@ -13,27 +22,48 @@ const Hero = () => {
           {/* Added padding to avoid edge touch */}
           <div className="w-full lg:w-1/2 flex flex-col items-center lg:items-start">
             <motion.h1
-              initial={{ x: -100, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 0.5, delay: 1 }}
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
               className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl text-neutral-300"
             >
               Luke Manyamazi
             </motion.h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
-              Software Engineer & FSD - Trainee
-            </span>
-            <p className="text-neutral-300 my-2 max-w-xl py-6 font-light tracking-tighter">
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
+            >
+              Software Engineer - Student
+            </motion.span>
+            <motion.span
+              variants={container(0.8)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-2xl tracking-tight text-transparent"
+            >
+              Full Stack Developer - Trainee
+            </motion.span>
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="text-neutral-300 my-2 max-w-xl py-6 font-light tracking-tighter"
+            >
               {HERO_CONTENT}
-            </p>
+            </motion.p>
           </div>
           <div className="w-full lg:w-1/2 lg:p-8 flex justify-center py-10">
-            <img
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.2 }}
               src={profilePic}
               width={400}
               height={400}
               alt="Luke Manyamazi on a red background"
-              className="rounded-2xl brightness-90 bg-gradient-to-tl from-black via-transparent to-black opacity-50 object-cover"
+              className="rounded-2xl brightness-75 bg-gradient-to-tl from-black via-transparent to-black opacity-50 object-cover"
             />
           </div>
         </div>
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -4,33 +4,87 @@ import { FaJsSquare } from "react-icons/fa";
 import { FaPython } from "react-icons/fa6";
 import { RiReactjsLine } from "react-icons/ri";
 import { FaNodeJs } from "react-icons/fa6";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <h2 className="my-20 text-center text-4xl text-neutral-300">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl text-neutral-300"
+      >
         Technologies
-      </h2>
-      <div className="flex flex-wrap items-center justify-center gap-4 text-4xl text-neutral-300">
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4 text-4xl text-neutral-300"
+      >
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <RiHtml5Line className="text-7xl text-yellow-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaCss3Alt className="text-7xl text-blue-600" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaJsSquare className="text-7xl text-orange-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaPython className="text-7xl text-yellow-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <RiReactjsLine className="text-7xl text-cyan-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaNodeJs className="text-7xl text-green-500" />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
