48a91791dbaa835bfb5deb1e0b19a9c95748a1a5
Luke Manyamazi <isirluke@gmail.com>
2025-02-22 22:14:40 +0200

    changed the scale

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,8 +19,8 @@ const Projects = () => {
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 1 }}
-              whileHover={{ scale: 1.5 }}
-              whileTap={{ scale: 1.1 }}
+              whileHover={{ scale: 1 }}
+              whileTap={{ scale: 1 }}
               className="w-full lg:w-1/4"
             >
               <motion.a href={project.link} target="_blank" rel="noreferrer">
